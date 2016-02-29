42990c8711e47b43fbc7380ef03c30bc55a54773
C80609A <c080609a@gmail.com>
2016-02-29 10:18:00 +0300

    minor

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 C80NewsTz::Engine.routes.draw do
   match 'news_guru', :to => 'application#guru', :via => :post
-  get '/rubrics/:rubric_slug' => 'site#rubric'
 end
--- lib/c80_news_tz/version.rb
+++ lib/c80_news_tz/version.rb
@@ -1,3 +1,3 @@
 module C80NewsTz
-  VERSION = "0.1.1.1"
+  VERSION = "0.1.1.2"
 end
