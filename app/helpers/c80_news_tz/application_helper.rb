1a6d1b9ff26bdad720d019b3862e87a3657c5cd3
C80609A <c080609a@gmail.com>
2016-03-07 17:48:00 +0300

    master: minor fix

--- app/helpers/c80_news_tz/application_helper.rb
+++ app/helpers/c80_news_tz/application_helper.rb
@@ -3,7 +3,7 @@ module C80NewsTz
   module ApplicationHelper
 
     def url_for_fact(fact)
-      "news/#{fact.slug}"
+      "#{root_url}news/#{fact.slug}"
     end
 
     def apph_url_for_rubric(rubric_slug)
@@ -11,15 +11,15 @@ module C80NewsTz
     end
 
     def apph_url_for_company(company_slug)
-      "companies/#{company_slug}"
+      "#{root_url}companies/#{company_slug}"
     end
     
     def apph_url_for_notice(notice_slug)
-      "notices/#{notice_slug}"
+      "#{root_url}notices/#{notice_slug}"
     end
     
     def apph_url_for_issue(issue_id)
-      "issues/#{issue_id}"
+      "#{root_url}issues/#{issue_id}"
     end
 
   end
