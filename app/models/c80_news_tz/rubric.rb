b0dca559f87c5b9ef7f58aa94330ab2cd5482695
C80609A <c080609a@gmail.com>
2016-03-07 11:35:42 +0300

    feature_spots: Rubric: добавил методы select_all_in_spots и pubs.

--- app/models/c80_news_tz/rubric.rb
+++ app/models/c80_news_tz/rubric.rb
@@ -23,5 +23,30 @@ module C80NewsTz
       # name_changed? || super
     end
 
+    # выдать массив рубрик, которые должны выводиться на главной
+    def self.select_all_in_spots
+      s = "
+        SELECT
+          `c80_news_tz_rubrics`.*,
+          `c80_news_tz_spots`.`id` AS `spot_id`
+        FROM `c80_news_tz_rubrics`
+          INNER JOIN `c80_news_tz_rubrics_spots` ON `c80_news_tz_rubrics_spots`.`rubric_id` = `c80_news_tz_rubrics`.`id`
+          INNER JOIN `c80_news_tz_spots` ON `c80_news_tz_spots`.`id` = `c80_news_tz_rubrics_spots`.`spot_id`
+        ORDER BY `spot_id`;
+      "
+      result = self.find_by_sql(s)
+      result
+
+    end
+
+    # выдать публикации из этой рубрики
+    def pubs(lim = nil)
+      if lim.nil?
+        C80NewsTz::Fact.joins(:rubrics).where(:c80_news_tz_rubrics => {:title => self.title})
+      else
+        C80NewsTz::Fact.joins(:rubrics).where(:c80_news_tz_rubrics => {:title => self.title}).limit(lim)
+      end
+    end
+
   end
 end
