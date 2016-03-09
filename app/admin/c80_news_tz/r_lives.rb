301fe292377bedfefbd48eb41cd7e882a62e75dd
C80609A <c080609a@gmail.com>
2016-03-09 14:02:14 +0300

    feature_reclame: шлифанул админку сущности "рекламодатель активный".

--- app/admin/c80_news_tz/r_lives.rb
+++ app/admin/c80_news_tz/r_lives.rb
@@ -4,41 +4,30 @@ ActiveAdmin.register C80NewsTz::RLive, :as => 'RLive' do
 
   before_filter :skip_sidebar!, :only => :index
 
-  # permit_params :fact_ids => []
-
-  config.sort_order = 'id_asc'
+  permit_params :r_advertiser_ids => []
 
   index do
-    #id_column
-    #column :title
-
-=begin
-    column :facts do |loc|
+    column :r_advertisers do |loc|
       # нарисуем список
-      list_items = ((loc.facts.map { |p|
-        "<li>• #{ p.title } <a href='/news/#{p.slug}' target='_blank'>[view]</a> <a href='/admin/facts/#{p.slug}/edit'>[edit]</a> </li>"
+      list_items = ((loc.r_advertisers.map { |p|
+        "<li>#{ p.title }</li>"
       }).join("")).html_safe
 
       "<ul>#{list_items}</ul>".html_safe
     end
-=end
-
     actions
   end
 
   form(:html => {:multipart => true}) do |f|
     f.inputs "Свойства" do
-      #f.input :title
 
-=begin
-      f.input :facts,
+      f.input :r_advertisers,
               :as => :select,
               :input_html => {:multiple => false},
               :include_blank => true,
               :member_label => Proc.new { |p|
                 p.title
               }
-=end
 
     end
     f.actions
