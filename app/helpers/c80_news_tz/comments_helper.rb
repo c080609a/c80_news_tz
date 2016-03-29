fd5b6c78d82984bc6aefd53d4d4b39e24d72acbd
C80609A <c080609a@gmail.com>
2016-03-29 20:03:32 +0300

    feature_comments: minor: переключился на обработку реального списка

--- app/helpers/c80_news_tz/comments_helper.rb
+++ app/helpers/c80_news_tz/comments_helper.rb
@@ -3,48 +3,53 @@ module C80NewsTz
 
     # рендер блока с комментариями к публикации
     def render_comments_block(blurb_or_fact)
+
       n = blurb_or_fact.comments.count
+      # list = [
+      #     {id: 12,
+      #      parent_id: 0,
+      #      user_name: 'Иван Николаевич',
+      #      time_published: '28 Март 2016 в 13:40',
+      #      user_profile_url: '/',
+      #      user_avatar_url: 'https://habrastorage.org/getpro/habr/avatars/97b/8ed/8d6/97b8ed8d6a0d24783dedb33192aeb604_small.jpg',
+      #      message: "все по поводу той же производительности ssd дисков.",
+      #      answers: [
+      #          {id: 22,
+      #           parent_id: 12,
+      #           user_name: 'Николай',
+      #           time_published: '28 Март 2016 в 15:15',
+      #           user_profile_url: '/',
+      #           user_avatar_url: 'https://habrastorage.org/getpro/habr/avatars/a0b/46e/dfa/a0b46edfa07bca1309a04541cde4cd1a.jpg',
+      #           message: "Кажется, ваш стэнд мы видели на NextCastleParty. Очень рад за вас, так держать."
+      #          },
+      #          {id: 23,
+      #           parent_id: 12,
+      #           user_name: 'Sample Dark',
+      #           time_published: '28 Март 2016 в 15:45',
+      #           user_profile_url: '/',
+      #           user_avatar_url: 'https://habrastorage.org/getpro/habr/avatars/0f0/a62/41f/0f0a6241fb94bacfd568f0df071ce01a_small.jpg',
+      #           message: "Бизнес суров, а игры — особенно применительно к теме краудфандинга — товар, который мы должны продать как можно большему числу людей."
+      #          }
+      #      ]
+      #     },
+      #     {id: 13,
+      #      parent_id: 0,
+      #      user_name: 'Sample Dark',
+      #      time_published: '28 Март 2016 в 13:45',
+      #      user_profile_url: '/',
+      #      user_avatar_url: 'https://habrastorage.org/getpro/habr/avatars/43e/9b4/de5/43e9b4de535dc4d0477313f70c63a4d2_small.jpg',
+      #      message: "Вот, например, большой открытый проект на 2.0: https://www.humhub.org/en",
+      #      answers: []
+      #     }
+      #
+      # ]
+
+      list = blurb_or_fact.comments
+
       render :partial => "c80_news_tz/comments/shared/comments_block",
              :locals => {
                  :comments_count => n,
-                 :comments_list => [
-                     {id: 12,
-                      parent_id: 0,
-                      user_name: 'Иван Николаевич',
-                      time_published: '28 Март 2016 в 13:40',
-                      user_profile_url: '/',
-                      user_avatar_url: 'https://habrastorage.org/getpro/habr/avatars/97b/8ed/8d6/97b8ed8d6a0d24783dedb33192aeb604_small.jpg',
-                      message: "все по поводу той же производительности ssd дисков.",
-                      answers: [
-                          {id: 22,
-                           parent_id: 12,
-                           user_name: 'Николай',
-                           time_published: '28 Март 2016 в 15:15',
-                           user_profile_url: '/',
-                           user_avatar_url: 'https://habrastorage.org/getpro/habr/avatars/a0b/46e/dfa/a0b46edfa07bca1309a04541cde4cd1a.jpg',
-                           message: "Кажется, ваш стэнд мы видели на NextCastleParty. Очень рад за вас, так держать."
-                          },
-                          {id: 23,
-                           parent_id: 12,
-                           user_name: 'Sample Dark',
-                           time_published: '28 Март 2016 в 15:45',
-                           user_profile_url: '/',
-                           user_avatar_url: 'https://habrastorage.org/getpro/habr/avatars/0f0/a62/41f/0f0a6241fb94bacfd568f0df071ce01a_small.jpg',
-                           message: "Бизнес суров, а игры — особенно применительно к теме краудфандинга — товар, который мы должны продать как можно большему числу людей."
-                          }
-                      ]
-                     },
-                     {id: 13,
-                      parent_id: 0,
-                      user_name: 'Sample Dark',
-                      time_published: '28 Март 2016 в 13:45',
-                      user_profile_url: '/',
-                      user_avatar_url: 'https://habrastorage.org/getpro/habr/avatars/43e/9b4/de5/43e9b4de535dc4d0477313f70c63a4d2_small.jpg',
-                      message: "Вот, например, большой открытый проект на 2.0: https://www.humhub.org/en",
-                      answers: []
-                     }
-
-                 ]
+                 :comments_list => list
              }
     end
 
