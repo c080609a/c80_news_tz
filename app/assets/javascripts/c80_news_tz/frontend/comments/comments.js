f6c7bd45cf316a5e899a3e94ec55f485d02c1851
C80609A <c080609a@gmail.com>
2016-03-30 10:30:23 +0300

    feature_comments: если пользователь неавторизован - рисовать сообщение и popup c авторизацией через соцсети

--- app/assets/javascripts/c80_news_tz/frontend/comments/comments.js
+++ app/assets/javascripts/c80_news_tz/frontend/comments/comments.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     var $form = $("#comment_form");
     var $comment_message = $("#comment_message");
@@ -16,29 +16,38 @@ $(document).ready(function() {
     $comment_message.blur(fCheckText);
     $comment_message.focus(fCheckText);
 
-    $form.submit(function(){
+    $form.submit(function () {
         $("input#comment_submit", $form)
-            .attr('disabled',true)
+            .attr('disabled', true)
             .addClass('loading');
     });
 
 });
 
+// показать форму "оставить комментарий к публикации"
 function comment_show_form() {
     //clearInterval(window.timer);
     //$(".comment_item .reply").removeClass("hidden");
-    var form = $("#comment_form");
-    //$("#preview_placeholder", form).hide();
-    //$('input[name="parent_id"]', form).val(0);
-    //$('input[name="comment_id"]', form).val(0);
-    //$(".time_left", form).text("");
-    $("#comment_form_placeholder").append(form);
-    //$(".edit,.delete", form).addClass("hidden");
-    $("input#comment_submit", form).removeClass("loading");
-    form.show();
-    $("#comment_message", form).val("").focus();
-    $('.comment_answers').addClass('hidden');
-    form.attr('action','/comments.js');
+    var $form = $("#comment_form");
+    var $placeholder = $("#comment_form_placeholder");
+    if ($form.length) {
+        //$("#preview_placeholder", form).hide();
+        //$('input[name="parent_id"]', form).val(0);
+        //$('input[name="comment_id"]', form).val(0);
+        //$(".time_left", form).text("");
+        $placeholder.append($form);
+        //$(".edit,.delete", form).addClass("hidden");
+        $("input#comment_submit", $form).removeClass("loading");
+        $form.show();
+        $("#comment_message", $form).val("").focus();
+        $('.comment_answers').addClass('hidden');
+        $form.attr('action', '/comments.js');
+    } else {
+        var $social = $("#social_buttons");
+        $social.attr('id','social_buttons_2');
+        $social.find('p').text('Чтобы иметь возможность оставлять комментарии, пожалуйста, авторизуйтесь через:');
+        $placeholder.append($social.clone());
+    }
     return false
 }
 
@@ -61,7 +70,7 @@ function comment_show_reply_form(comment_id) {
         $("#comment_message", form).val("").focus();
         //_activate_comment_body(comment_id);
         //mention_autocomplete($("#comment_message"));
-        form.attr('action','/comments');
+        form.attr('action', '/comments');
     } else {
         //var placeholder = $("#expired_placeholder");
         //$("#comment_" + comment_id + " > .comment_body > .reply").addClass("hidden");
@@ -79,7 +88,7 @@ function _show_comment_answers($comment_answers) {
 
 // подсветить коммент, в который вставлена reply_form
 /*
-function _activate_comment_body(comment_id) {
-    $('.comment_body').removeClass('active');
-    $("#comment_" + comment_id + " > .comment_body").addClass('active');
-}*/
+ function _activate_comment_body(comment_id) {
+ $('.comment_body').removeClass('active');
+ $("#comment_" + comment_id + " > .comment_body").addClass('active');
+ }*/
