f87e1bb5154d3be8e6648d6039769a907cf29e1f
C80609A <c080609a@gmail.com>
2016-03-30 11:39:07 +0300

    feature_comments: клик по иконке 'комментарии' ведёт на страницу в блок 'комментарии'

--- app/assets/javascripts/c80_news_tz/frontend/comments/comments_icon_animate_scroll.js
+++ app/assets/javascripts/c80_news_tz/frontend/comments/comments_icon_animate_scroll.js
@@ -1,12 +1,16 @@
 $(document).ready(function() {
-    $('.fact_time_issue').find('div.comments').click(function () {
+    $('.fact_time_issue').find('a.comments').click(function (e) {
+        e.preventDefault();
+        e.stopImmediatePropagation();
+
         $('html, body').animate(
             {
             scrollTop: $("#comments_block").offset().top
             }, {
                 duration: 1000,
                 easing: "easeOutSine"
-            });
+            }
+        );
 
     });
 });
